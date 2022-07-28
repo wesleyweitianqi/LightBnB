7ce2927143cc747507d49bafaae02e9253419146
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-28 19:36:52 -0400

    add property by form submission

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -148,9 +148,31 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+  const arr = Object.values(property);
+  
+  let values = []
+  for (let i in arr) {
+    values.push(`$${Number(i)+1}`)
+  }
+  console.log(values.toString());
+  
+  console.log(arr);
+  return pool.query(`INSERT INTO properties(
+    title, 
+    description,
+    number_of_bedrooms,
+    number_of_bathrooms,
+    parking_spaces,
+    cost_per_night,
+    thumbnail_photo_url,
+    cover_photo_url,
+    street,
+    country,
+    city,
+    province,
+    post_code,
+    owner_id) 
+    VALUES(${values.toString()})`, arr).then(res => res.rows[0]).catch((err) => console.error(err));
+};
 exports.addProperty = addProperty;
+// $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14
